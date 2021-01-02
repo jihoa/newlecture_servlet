57122997c20c20f6f6a5e4838f198074eec06bd3
kimth007kim <kimth007kim@naver.com>
2021-01-03 04:18:50 +0900

    서블릿/JSP 12 어노테이션을 이용한 URL 매핑

---
+++ workspace/JSPPrj/src/com/newlecture/web/Nana.java
@@ -0,0 +1,18 @@
+package com.newlecture.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Nana extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		PrintWriter out = resp.getWriter();
+		out.println("hello!");
+	}
+}
