e656f3578bbcdb187e28c07f8372df4f646a03fa
kimth007kim <kimth007kim@naver.com>
2021-02-26 16:01:47 +0900

    JDBC 프로그래밍 06 완료

--- workspace/JDBCPrj/src/ex1/Program.java
+++ workspace/JDBCPrj/src/ex1/Program.java
@@ -5,22 +5,42 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Date;
 
 public class Program {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		String url = "jdbc:oracle:thin:@192.168.137.1:1521/xepdb1";
+		String url = "jdbc:oracle:thin:@192.168.77.1:1521/xepdb1";
 		String sql = "SELECT * FROM NOTICE";
 		
 		Class.forName("oracle.jdbc.driver.OracleDriver");
-		Connection con= DriverManager.getConnection(url,"newlec","1234");
+		Connection con= DriverManager.getConnection(url,"NEWLEC","1234");
 		Statement st = con.createStatement();
 		ResultSet rs= st.executeQuery(sql);
 		
-		if(rs.next()) {
-			String title = rs.getString("title");
-			System.out.println();
-		}
+
+		
+		while(rs.next()) {
+		int id =rs.getInt("ID");
+		String title = rs.getString("TITLE");
+		String writer = rs.getString("WRITER_ID");
+		Date regDate = rs.getDate("REGDATE");
+		String content = rs.getString("CONTENT");
+		int hit =rs.getInt("hit");
+		System.out.printf("id:%d, title:%s, writerId:%s, regDate:%s, content:%s, hit:%d\n",id,title,writer,regDate,content,hit);
+	}
+		
+		
+//		if(rs.next()) {
+//			int id =rs.getInt("ID");
+//			String title = rs.getString("TITLE");
+//			String writer = rs.getString("WRITER_ID");
+//			Date regDate = rs.getDate("REGDATE");
+//			String content = rs.getString("CONTENT");
+//			int hit =rs.getInt("hit");
+//			System.out.printf("id:%d, title:%s, writerId:%s, regDate:%s, content:%s, hit:%d\n",id,title,writer,regDate,content,hit);
+//		}
+//		
 		rs.close();
 		st.close();
 		con.close();
